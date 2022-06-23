3cc5a049a1c23cc18fa9c61585fad508cec91307
rdmadeira <rdmadeira2@gmail.com>
2022-06-22 23:04:45 -0300

    Cambiohtml div por label, se agrego estilos

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 window.onload = ()=> {
     const lightSwitch = document.getElementById('light-switch-div');
     const darkSwitch = document.getElementById('dark-switch-div');
-    lightSwitch.addEventListener('click', lightMode);        
+    lightSwitch.addEventListener('click', lightMode);   const main = document.querySelector('main');
+    const buttonsDiv = document.querySelector('.buttons-div');
+    const buttons = document.querySelectorAll('.button');
     darkSwitch.addEventListener('click', darkMode);
     darkMode();
     function lightMode(){
         let checkedInput = document.querySelector('input[type="radio"]:checked');
         if(checkedInput.value == 'dark'){
-            lightSwitch.style.backgroundColor = 'var(--cadet-grey)';
+            lightSwitch.style.backgroundColor = 'hsl(0deg 37% 85%)';
             document.querySelector('#light-switch-div img').setAttribute('src','./images/sun-light.svg');
             document.querySelector('#dark-switch-div img').setAttribute('src','./images/moon-dark.svg');
             darkSwitch.removeAttribute('style');
         }
+        main.setAttribute('class','light-main');
+        buttonsDiv.setAttribute('class','buttons-div buttons-div-light');
+        buttons.forEach( item => item.setAttribute('class','button light'));
     }
     function darkMode(){
         let checkedInput = document.querySelector('input[type="radio"]');
@@ -21,5 +26,9 @@ window.onload = ()=> {
             document.querySelector('#dark-switch-div img').setAttribute('src','./images/moon-light.svg');
             lightSwitch.removeAttribute('style');
         }
+        main.setAttribute('class','dark-main');
+        buttonsDiv.setAttribute('class','buttons-div buttons-div-dark');
+        buttons.forEach( item => item.setAttribute('class','button dark'));
     }
+    
 }
