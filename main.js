6189b96bd9448e5e9ad41a21c18d4c801d4eba8e
rdmadeira <rdmadeira2@gmail.com>
2022-06-26 16:13:58 -0300

    se adiciono display background

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ window.onload = ()=> {
     const buttons = document.querySelectorAll('.button');
     const operationDisplay = document.getElementById('operation-display');
     const resultDisplay = document.getElementById('result-display');
+    const displayDiv = document.getElementById('display-div');
     darkSwitch.addEventListener('click', darkMode);
     darkMode();
     function lightMode(){
         let checkedInput = document.querySelector('input[type="radio"]:checked');
         if(checkedInput.value == 'dark'){
-            lightSwitch.style.backgroundColor = 'hsl(0deg 37% 85%)';
+            lightSwitch.style.backgroundColor = 'rgb(245 222 179)';
             document.querySelector('#light-switch-div img').setAttribute('src','./images/sun-light.svg');
+            document.querySelector('#switch').setAttribute('class','switch light');
             document.querySelector('#dark-switch-div img').setAttribute('src','./images/moon-dark.svg');
             darkSwitch.removeAttribute('style');
         }
@@ -21,11 +23,13 @@ window.onload = ()=> {
         buttons.forEach( item => item.setAttribute('class','button light'));
         operationDisplay.setAttribute('class','light-font');
         resultDisplay.setAttribute('class', 'light-font');
+        displayDiv.setAttribute('class','display-div-light');
     }
     function darkMode(){
         let checkedInput = document.querySelector('input[type="radio"]');
         if(checkedInput.value == 'light'){
             darkSwitch.style.backgroundColor = 'var(--cadet-grey)';
+            document.querySelector('#switch').setAttribute('class','switch dark');
             document.querySelector('#light-switch-div img').setAttribute('src','./images/sun-dark.svg');
             document.querySelector('#dark-switch-div img').setAttribute('src','./images/moon-light.svg');
             lightSwitch.removeAttribute('style');
@@ -35,6 +39,7 @@ window.onload = ()=> {
         buttons.forEach( item => item.setAttribute('class','button dark'));
         operationDisplay.setAttribute('class','dark-font');
         resultDisplay.setAttribute('class', 'dark-font');
+        displayDiv.removeAttribute('class');
     }
     let operationText = '';
     let operationCopy = '';
